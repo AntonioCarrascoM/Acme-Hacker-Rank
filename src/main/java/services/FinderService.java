fc859dbefd43bf032c0e470fa9c367192443a8ec
Sermor <sermor95@gmail.com>
2019-04-04 18:12:48 +0200

    FinderService and HackerService
    I can not check finderService until we will deploy it in localhost

--- src/main/java/services/FinderService.java
+++ src/main/java/services/FinderService.java
@@ -3,6 +3,7 @@ package services;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.FinderRepository;
+import security.Authority;
+import domain.Actor;
 import domain.Finder;
 import domain.Hacker;
 import domain.Position;
@@ -21,16 +24,19 @@ public class FinderService {
 	//Managed service
 
 	@Autowired
-	private FinderRepository	finderRepository;
-
+	private FinderRepository		finderRepository;
 
 	//Supporting service
 
-	//	@Autowired
-	//	private ActorService			actorService;
-	//
-	//	@Autowired
-	//	private ConfigurationService	configurationService;
+	@Autowired
+	private ActorService			actorService;
+
+	@Autowired
+	private HackerService			hackerService;
+
+	@Autowired
+	private ConfigurationService	configurationService;
+
 
 	//Simple CRUD methods --------------------------------
 
@@ -48,96 +54,79 @@ public class FinderService {
 	public Collection<Finder> findAll() {
 		return this.finderRepository.findAll();
 	}
-	//
-	//	public Finder save(final Finder f) {
-	//		Assert.notNull(f);
-	//		//Assertion that the user modifying this finder has the correct privilege.
-	//		Assert.isTrue(f.getId() == this.findPrincipalFinder().getId());//this.findPrincipalFinder().getId()
-	//		//If all fields of the finder are null, the finder returns the entire listing of available tasks.
-	//		f.setMoment(new Date(System.currentTimeMillis() - 1));
-	//		final Finder saved = this.finderRepository.save(f);
-	//
-	//		return saved;
-	//	}
-	//
-	//	public void delete(final Finder f) {
-	//		Assert.notNull(f);
-	//
-	//		//Assertion that the user deleting this finder has the correct privilege.
-	//		Assert.isTrue(this.actorService.findByPrincipal().getId() == this.memberService.memberByFinder(f.getId()).getId());
-	//
-	//		this.finderRepository.delete(f);
-	//	}
-	//
-	//	public Finder findPrincipalFinder() {
-	//		final Actor a = this.actorService.findByPrincipal();
-	//		final Authority auth = new Authority();
-	//		auth.setAuthority(Authority.MEMBER);
-	//		Assert.isTrue(a.getUserAccount().getAuthorities().contains(auth));
-	//
-	//		final Member m = (Member) this.actorService.findOne(a.getId());
-	//		Finder fd = new Finder();
-	//		if (m.getFinder() == null) {
-	//			fd = this.create();
-	//			final Finder saved = this.finderRepository.save(fd);
-	//			m.setFinder(saved);
-	//			this.memberService.save(m);
-	//			return saved;
-	//		} else
-	//			return m.getFinder();
-	//	}
-	//
-	//	public Collection<Parade> find(final Finder finder) {
-	//		Assert.notNull(finder);
-	//		Collection<Parade> parades = new ArrayList<>();
-	//		Collection<Parade> results = new ArrayList<>();
-	//		String keyWord = finder.getKeyWord();
-	//		Date minimumDate = finder.getMinimumDate();
-	//		Date maximumDate = finder.getMaximumDate();
-	//
-	//		if (finder.getKeyWord() == null)
-	//			keyWord = "";
-	//		if (finder.getMinimumDate() == null)
-	//			minimumDate = new Date(631152000L);
-	//		if (finder.getMaximumDate() == null)
-	//			maximumDate = new Date(2524694400000L);
-	//
-	//		final Collection<Parade> firstResults = this.findParades(keyWord, minimumDate, maximumDate);
-	//		Collection<Parade> secondResults = new ArrayList<>();
-	//
-	//		if (finder.getArea() != null)
-	//			secondResults = this.paradeService.paradesByArea(finder.getArea().getId());
-	//		else
-	//			secondResults = this.paradeService.findAll();
-	//
-	//		parades = this.intersection(firstResults, secondResults);
-	//		results = this.limitResults(parades);
-	//		return results;
-	//	}
-	//
-	//	public Collection<Parade> limitResults(final Collection<Parade> parades) {
-	//		Collection<Parade> results = new ArrayList<>();
-	//		final int maxResults = this.configurationService.findAll().iterator().next().getMaxFinderResults();
-	//		if (parades.size() > maxResults)
-	//			results = new ArrayList<Parade>(((ArrayList<Parade>) parades).subList(0, maxResults));
-	//		else
-	//			results = parades;
-	//		return results;
-	//	}
-	//
-	//	private Collection<Parade> intersection(final Collection<Parade> a, final Collection<Parade> b) {
-	//		final Collection<Parade> c = new ArrayList<>();
-	//		final Collection<Parade> mayor = a.size() > b.size() ? a : b;
-	//		for (final Parade f : mayor)
-	//			if (a.contains(f) && b.contains(f))
-	//				c.add(f);
-	//		return c;
-	//	}
-	//
-	//	//Search parades 
-	//	public Collection<Parade> findParades(final String keyWord, final Date minimunDate, final Date maximunDate) {
-	//		return this.finderRepository.findParades(keyWord, minimunDate, maximunDate);
-	//	}
+
+	public Finder save(final Finder f) {
+		Assert.notNull(f);
+		//Assertion that the user modifying this finder has the correct privilege.
+		Assert.isTrue(f.getId() == this.findPrincipalFinder().getId());//this.findPrincipalFinder().getId()
+		//If all fields of the finder are null, the finder returns the entire listing of available tasks.
+		f.setMoment(new Date(System.currentTimeMillis() - 1));
+		final Finder saved = this.finderRepository.save(f);
+
+		return saved;
+	}
+
+	public void delete(final Finder f) {
+		Assert.notNull(f);
+
+		//Assertion that the user deleting this finder has the correct privilege.
+		Assert.isTrue(this.actorService.findByPrincipal().getId() == this.hackerService.hackerByFinder(f.getId()).getId());
+
+		this.finderRepository.delete(f);
+	}
+
+	public Finder findPrincipalFinder() {
+		final Actor a = this.actorService.findByPrincipal();
+		final Authority auth = new Authority();
+		auth.setAuthority(Authority.HACKER);
+		Assert.isTrue(a.getUserAccount().getAuthorities().contains(auth));
+
+		final Hacker h = (Hacker) this.actorService.findOne(a.getId());
+		Finder fd = new Finder();
+		if (h.getFinder() == null) {
+			fd = this.create();
+			final Finder saved = this.finderRepository.save(fd);
+			h.setFinder(saved);
+			this.hackerService.save(h);
+			return saved;
+		} else
+			return h.getFinder();
+	}
+
+	public Collection<Position> find(final Finder finder) {
+		Assert.notNull(finder);
+
+		Collection<Position> results = new ArrayList<>();
+
+		String keyWord = finder.getKeyWord();
+		Date specificDeadline = finder.getSpecificDeadline(), maxDeadline = finder.getMaxDeadline();
+		Double minimunSalary = finder.getMinimumSalary();
+
+		if (finder.getKeyWord() == null)
+			keyWord = "";
+		if (specificDeadline == null)
+			specificDeadline = new Date(631152000L);
+		if (maxDeadline == null)
+			maxDeadline = new Date(2524694400000L);
+		if (minimunSalary == null)
+			minimunSalary = 0.;
+
+		final Collection<Position> positions = this.finderRepository.findPosition(keyWord, specificDeadline, maxDeadline, minimunSalary);
+
+		results = this.limitResults(positions);
+		return results;
+	}
+
+	public Collection<Position> limitResults(final Collection<Position> positions) {
+		Collection<Position> results = new ArrayList<>();
+		final int maxResults = this.configurationService.findAll().iterator().next().getMaxFinderResults();
+		if (positions.size() > maxResults)
+			results = new ArrayList<Position>(((ArrayList<Position>) positions).subList(0, maxResults));
+		else
+			results = positions;
+		return results;
+	}
+
 	//
 	//	//The minimum, the maximum, the average, and the standard deviation of the number of results in the finders.
 	//	public Double[] minMaxAvgStddevResultsFinders() {
